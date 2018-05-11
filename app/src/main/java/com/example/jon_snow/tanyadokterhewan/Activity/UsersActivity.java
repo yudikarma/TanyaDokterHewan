f68b6d85897bc99e37f1b86b7e965ab8e6aaac2b
yudikarma <yudikarmateknik@gmail.com>
2018-05-12 02:02:35 +0700

    - Try recycleview alluser.activity

--- app/src/main/java/com/example/jon_snow/tanyadokterhewan/Activity/UsersActivity.java
+++ app/src/main/java/com/example/jon_snow/tanyadokterhewan/Activity/UsersActivity.java
@@ -6,22 +6,28 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 //import android.widget.Toolbar;
 
 import com.example.jon_snow.tanyadokterhewan.R;
 import com.example.jon_snow.tanyadokterhewan.Users;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
-public class UsersActivity extends AppCompatActivity {
+public class UsersActivity extends AppCompatActivity{
     private Toolbar mToolbar;
-    private RecyclerView mRecyclerView;
+    private RecyclerView mListView;
     private String nambahaja;
     private DatabaseReference mDatabaseReference;
-
+    //private FirebaseListAdapter adapter;
+    private FirebaseDatabase mFirebaseDatabase;
+    private FirebaseRecyclerAdapter<Users,UserviewHolder> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,14 +36,42 @@ public class UsersActivity extends AppCompatActivity {
         mDatabaseReference = FirebaseDatabase.getInstance().getReference().child("Users");
 
         mToolbar = (Toolbar) findViewById(R.id.user_appbar);
+        setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("ALL User");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        setSupportActionBar(mToolbar);
-        mRecyclerView = (RecyclerView) findViewById(R.id.user_list);
-        mRecyclerView.setHasFixedSize(true);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mListView = (RecyclerView) findViewById(R.id.user_list);
+        mListView.setHasFixedSize(true);
+        mListView.setLayoutManager(new LinearLayoutManager(this));
+        Query query = FirebaseDatabase.getInstance().getReference().child("Users").limitToLast(50);
 
 
+        FirebaseRecyclerOptions<Users> options =
+                new FirebaseRecyclerOptions.Builder<Users>()
+                        .setQuery(query, Users.class)
+                        .setLifecycleOwner(this)
+                        .build();
+
+        adapter = new FirebaseRecyclerAdapter<Users, UserviewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull UserviewHolder holder, int position, @NonNull Users model) {
+                holder.setNama(model.getDisplay_name());
+
+            }
+
+
+            @NonNull
+            @Override
+            public UserviewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View mView = LayoutInflater.from(parent.getContext()).inflate(R.layout.user_singgle_layout,parent,false);
+                return new UserviewHolder(mView);
+
+
+
+            }
+
+        };
+        mListView.setAdapter(adapter);
 
 
     }
@@ -47,14 +81,33 @@ public class UsersActivity extends AppCompatActivity {
         super.onStart();
 
 
+      adapter.startListening();
+
+
+
     }
 
-    public class UsersViewHolder extends RecyclerView.ViewHolder{
-        View mView;
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
 
-        public UsersViewHolder(View itemView) {
+    public class UserviewHolder extends RecyclerView.ViewHolder {
+        View mView;
+        public UserviewHolder(View itemView) {
             super(itemView);
             mView = itemView;
+
+
+        }
+        public void setNama(String display_name){
+            TextView mdisplayname = (TextView) mView.findViewById(R.id.user_singgle_name);
+            mdisplayname.setText(display_name);
+
         }
     }
+
+
+
 }
